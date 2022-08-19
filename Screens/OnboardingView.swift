bde577bc349f5d8989d509cefa2b4644c928316e
bimo.ez <bimo.ez@kakaomobility.com>
2022-08-20 01:53:10 +0900

    온보딩 화면 구현 (스와이프 에니메이션 제외)

--- Screens/OnboardingView.swift
+++ Screens/OnboardingView.swift
@@ -12,21 +12,99 @@ struct OnboardingView: View {
     @AppStorage(AppStorageIdentifier.onboarding.rawValue) private var isActiveOnboarding: Bool = true
     
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding")
-                .font(.largeTitle)
-            
-            Button(action: {
-                isActiveOnboarding = false
-            }) {
-                Text("Start")
-            }
-        } //: VStack
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            VStack(spacing: 20) {
+                
+                Spacer()
+                
+                // MARK: - Header
+                VStack(spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                    It's not how much we give but
+                    how much love we put into giving.
+                    """)
+                        .font(.title3)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(10)
+                } //: Header
+                
+                // MARK: - Center
+                ZStack {
+                    
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                } //: Center
+                
+                Spacer()
+                
+                // MARK: - Footer
+                ZStack {
+                    ZStack {
+                        Capsule()
+                            .fill(Color.white.opacity(0.2))
+                        Capsule()
+                            .fill(Color.white.opacity(0.2))
+                            .padding(8)
+                    } //: background
+                    
+                    Text("Get Start")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isActiveOnboarding = false
+                        }
+                        
+                        Spacer()
+                    } //: Circle Button
+                    
+                } //: Fotter
+                .frame(height: 80, alignment: .center)
+                .padding(8)
+                
+                
+            } //: VStack
+        } //: ZStack
     }
 }
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingView()
+        Group {
+            OnboardingView()
+        }
     }
 }
