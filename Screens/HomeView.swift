f91da8c96cf52442fc609722115dedb20bad2a4b
bimo.ez <bimo.ez@kakaomobility.com>
2022-08-20 16:41:52 +0900

    홈화면 GUI 수정

--- Screens/HomeView.swift
+++ Screens/HomeView.swift
@@ -13,14 +13,45 @@ struct HomeView: View {
     
     var body: some View {
         VStack(spacing: 20) {
-            Text("Home")
-                .font(.largeTitle)
             
+            Spacer()
+            
+            // MARK: - Header
+            ZStack {
+                CircleGroupView(shapeColor: .gray, shapeOpacity: 0.1)
+                
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                .padding()
+            } //: Header
+            
+            // MARK: - Center
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            Spacer()
+            
+            // MARK: - Footer
             Button(action: {
                 isActiveOnboarding = true
             }) {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                
                 Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
             }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+            .padding()
+            
         } //: VStack
     }
 }
